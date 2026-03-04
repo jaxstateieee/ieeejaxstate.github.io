c117fab26a4111a918df62785a3f0ef009f29408
sakibhossenrajon <srajon@stu.jsu.edu>
2026-03-03 19:11:10 -0600

    Changed the Hero Image text and rearranged calendar

--- script.js
+++ script.js
@@ -319,9 +319,9 @@ document.addEventListener('DOMContentLoaded', () => {
   const navbar = document.querySelector('.navbar');
   window.addEventListener('scroll', () => {
     if (window.scrollY > 80) {
-      navbar.classList.add('scrolled');
-    } else {
       navbar.classList.remove('scrolled');
+    } else {
+      navbar.classList.add('scrolled');
     }
   });
 
